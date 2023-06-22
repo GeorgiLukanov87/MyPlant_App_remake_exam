79c12d827cda9671717df7041fa95c9c0a537831
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-22 13:43:59 +0200

    models

--- MyPlant_App_remake_exam/my_web/models.py
+++ MyPlant_App_remake_exam/my_web/models.py
@@ -1,3 +1,60 @@
+from django.core import validators
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+def validate_first_name(value):
+    if not value[0].isupper():
+        raise ValidationError('Your name must start with a capital letter!')
+
+
+def validate_plant_name(value):
+    if not value.isalpha():
+        raise ValidationError('Plant name should contain only letters!')
+
+
+class Profile(models.Model):
+    username = models.CharField(
+        max_length=10,
+        validators=[
+            validators.MinLengthValidator(2, message='It should consist of a minimum of 2 characters!')
+        ]
+    )
+
+    first_name = models.CharField(
+        max_length=20,
+        validators=[validate_first_name],
+    )
+
+    last_name = models.CharField(
+        max_length=20,
+        validators=[validate_first_name],
+    )
+
+    profile_picture = models.URLField(
+        blank=True,
+        null=True,
+    )
+
+
+class Plant(models.Model):
+    PLANT_TYPE_CHOICES = (
+        ('Outdoor Plants', 'Outdoor Plants'),
+        ('Indoor Plants', 'Indoor Plants'),
+    )
+    plant_type = models.CharField(
+        max_length=14,
+        choices=PLANT_TYPE_CHOICES,
+    )
+
+    name = models.CharField(
+        max_length=20,
+        validators=[
+            validators.MinLengthValidator(2, message='It should consist of a minimum of 2 characters!'),
+            validate_plant_name,
+        ]
+    )
+
+    image_url = models.URLField()
+    description = models.TextField()
+    price = models.FloatField()
--- MyPlant_App_remake_exam/settings.py
+++ MyPlant_App_remake_exam/settings.py
@@ -15,6 +15,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'MyPlant_App_remake_exam.my_web',
 ]
 
 MIDDLEWARE = [
